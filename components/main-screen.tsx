60a943c560ab1205a8b5a7583e23befb1a04d95f
Isabellilino <isabellilino2008@gmail.com>
2025-09-01 11:18:12 +0000

    Aula 01/09/2025

---
+++ components/main-screen.tsx
@@ -0,0 +1,11 @@
+
+const MainScreen = () =>{
+    return (
+        <main>
+            <h1>Exemplo de Aplicação Next</h1>
+            <p>Teste de Implamtação</p>
+        </main>
+    );
+}
+
+export default MainScreen;
